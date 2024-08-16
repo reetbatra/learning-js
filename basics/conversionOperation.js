2f05332e28791694e57e197f34b68ce88b07e7e6
reetbatra <reetbatra25@gmail.com>
2024-08-16 22:48:33 +0530

    data types summary

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 //if score = null, then valueInNumber = 0
 //if score = undefined, then valueInNumber = NaN
@@ -13,9 +13,28 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn, 'bool');
+//console.log(booleanIsLoggedIn, 'bool');
 
 let someNumber =33
 let strNumber = String(someNumber);
-console.log(typeof someNumber);
+////console.log(typeof someNumber);
 console.log(someNumber);
+
+//OPERATIONS
+
+let value =3
+let negValue = -value
+console.log(negValue)
+
+let num1, num2, num3
+num1= num2 = num3 = 2+2
+
+let gameCounter =100
+gameCounter;
+console.log(gameCounter);
+
+
+//make sure data types are same while comparing >=, <=, ==, etc
+
+
+
---
+++ basics/dataTypesNotes.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types= string, number, boolean, null, undefined, symbol, BigInt
+
+const scrore = 100
+const scroreValue = 100.3
+const isLoggedInJ = false
+const outsideTemp = null
+
+let userEmail;
+
+const id= Symbol('123')
+const anotherId = Symbol('123')
+
+
+// Reference (Non primitive)
+
+// Arrays, Objects, functions
+
+//Memory
+
+//Stack  (primitive), Heap (Non-primitive)
+
+let myName = 'reetbatra'
+anotherName = 'effcdsc'
+
+let userOne ={
+    email: 'wedefefd',
+    upi: 'ewfccf'
+}
+
+let userTwo = userOne
+
+console.log(userOne.email)
+console.log(userTwo.email)
